4b601e2020b4599a25955b75ac9912eb55f88419
jovinull <felipejovinogamerplay@gmail.com>
2025-10-25 18:13:35 -0300

    feat: adicionando a lib (do lumi)

---
+++ src/lib/lumi.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@lumi.new/sdk'
+
+export const lumi = createClient({
+  projectId: 'p372810674982506496',
+  apiBaseUrl: 'https://api.lumi.new',
+  authOrigin: 'https://auth.lumi.new',
+})
